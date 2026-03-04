Fix: Correctig 5D attention mask to 4D

models/attention_layer.py
@@ -108,12 +108,12 @@ class GroupedQueryAttention(nn.Module):
 
         # Attention
         attn_scores = queries @ keys.transpose(2, 3)
-        print("Attn Scores Shape: ", attn_scores.shape)
-        print("Attn Mask Shape: ", mask.shape)
+        # print("Attn Scores Shape: ", attn_scores.shape)
+        # print("Attn Mask Shape: ", mask.shape)
         attn_scores = attn_scores.masked_fill(mask, -torch.inf)
         attn_weights = torch.softmax(attn_scores, dim=-1)
 
-        print("Context Shape: ", (attn_weights @ values).shape)
-        print("Context Transpose Shape: ", (attn_weights @ values).transpose(1, 2).shape)
+        # print("Context Shape: ", (attn_weights @ values).shape)
+        # print("Context Transpose Shape: ", (attn_weights @ values).transpose(1, 2).shape)
         context = (attn_weights @ values).transpose(1, 2).reshape(b, num_tokens, self.d_out)
         return self.out_proj(context)

models/gemma_new.py
@@ -161,8 +161,8 @@ class Gemma3Model(nn.Module):
             dataset_mask_hidden = ~attention_mask.to(torch.bool)
             
             # Broadcast and merge using logical OR
-            mask_global = mask_global | dataset_mask_hidden.unsqueeze(1).unsqueeze(1)
-            mask_local = mask_local | dataset_mask_hidden.unsqueeze(1).unsqueeze(1)
+            mask_global = mask_global | dataset_mask_hidden.unsqueeze(1)
+            mask_local = mask_local | dataset_mask_hidden.unsqueeze(1)
 
         for block in self.blocks:
             x = block(